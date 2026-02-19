7c68c9d0fbacdd661a0e0be4371a6297392c7426
taufikqhakim-kudos <taufikqhakim@gmail.com>
2026-02-19 15:39:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.markdown("""
     .main { background-color: #f5f7f9; }
     .stMetric { background-color: #ffffff; padding: 15px; border-radius: 10px; box-shadow: 0 2px 4px rgba(0,0,0,0.05); }
     </style>
-    """, unsafe_allow_value=True)
+    """, unsafe_allow_html=True)
 
 st.title("🏥 Profit Klinik TPMD")
 st.info("Gunakan aplikasi ini untuk simulasi keuntungan paket pemeriksaan.")
